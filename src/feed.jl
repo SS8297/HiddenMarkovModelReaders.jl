5e185242c4ebcbc396911c65e43d1f137bc18c39
Shuai Shao <shuai1997@hotmail.se>
2023-05-15 10:53:12 +0200

    Parameterized feed.jl distance function

--- src/feed.jl
+++ src/feed.jl
@@ -3,7 +3,7 @@
 "Feed on hidden Markov model object."
 function feed!(∫::HMM, frame::I, ɒ::M; params::HMMParams) where I <: Int64 where M <: Matrix{F} where F <: Float64
   for ι ∈ eachindex(∫.model)
-    plus = euclideanDistance(∫.data[ι], ɒ[frame, :])
+    plus = params.distance(∫.data[ι], ɒ[frame, :])
     for ο ∈ eachindex(∫.model)
       lpen = copy(params.penalty)
       if (ο == ι) lpen = 0 end
