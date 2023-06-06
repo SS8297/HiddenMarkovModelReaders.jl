ea22c44908d43ab0dcdabbd9437cc63bffe62c06
Shuai Shao <shuai1997@hotmail.se>
2023-06-06 04:10:05 +0200

    Fixed signature for setup.jl

--- src/setup.jl
+++ src/setup.jl
@@ -31,7 +31,7 @@ Initiate hidden Markov model object.
 
 See also: [`process!`](@ref), [`HMM`](@ref), [`HMMParams`](@ref)
 """
-function setup(ɒ::A) where A ::Array{Float64, 3}
+function setup(ɒ::A) where A <: Array{Float64, 3}
   hmm = HMM([zeros(ssize(ɒ, 2), size(ɒ, 3))], [zeros(size(ɒ, 1) + 1)], Array{Float64}(undef, 0))
   reset!(hmm)
   return hmm
